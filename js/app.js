164cae3ac1ec8cc5ec61af30fbf7b5667bbb26cc
Alan Parley <alanparley@hotmail.com>
2020-10-16 21:27:36 +0100

    added css

--- js/app.js
+++ js/app.js
@@ -15,3 +15,32 @@ const handleNewItemFormSubmit = function (event) {
 
     event.target.reset();
 }
+
+const createBucketListItem = function (form) {
+    const bucketListItem = document.createElement('li');
+    bucketListItem.classList.add('bucket-list-item');
+
+    const city = document.createElement('h1');
+    city.textContent = form.city.value;
+    bucketListItem.appendChild(city);
+
+    const country = document.createElement('h3');
+    country.textContent = form.country.value;
+    bucketListItem.appendChild(country);
+
+    const continent = document.createElement('p');
+    continent.textContent = form.continent.value;
+    bucketListItem.appendChild(continent);
+
+    const visited = document.createElement('p');
+    visited.textContent = form.visited.value;
+    bucketListItem.appendChild(visited)
+
+    return bucketListItem;
+
+}
+
+const handleDeleteAllClick = function (event) {
+    const bucketList = document.querySelector('#bucket-list');
+    bucketList.innerHTML = '';
+}
